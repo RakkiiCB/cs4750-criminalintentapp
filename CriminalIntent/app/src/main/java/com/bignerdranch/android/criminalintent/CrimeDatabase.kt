2db87b0f5443da313a9a8eb5b7be03d904d7f8d7
RAKKII-san <schuang2001@gmail.com>
2023-09-29 13:25:11 -0700

    End of Chapter 17: Taking Pictures with Intents

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [ Crime::class ], version=1)
+@Database(entities = [ Crime::class ], version = 3)
 @TypeConverters(CrimeTypeConverters::class)
 abstract class CrimeDatabase : RoomDatabase() {
     abstract fun crimeDao(): CrimeDao
@@ -16,6 +16,14 @@ val migration_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL(
             "ALTER TABLE Crime ADD COLUMN suspect TEXT NOT NULL DEFAULT ''"
+        )
+    }
+}
+
+val migration_2_3 = object : Migration(2, 3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL(
+            "ALTER TABLE Crime ADD COLUMN photoFileName TEXT"
         )
     }
 }
--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -19,7 +19,7 @@ class CrimeRepository private constructor(
             CrimeDatabase::class.java,
             DATABASE_NAME
         )
-        .addMigrations(migration_1_2)
+        .addMigrations(migration_1_2, migration_2_3)
         .build()
 
     fun getCrimes(): Flow<List<Crime>>
